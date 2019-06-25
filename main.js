73446eb70f684ed9994bb4764e7301b89d78a9e1
=Jarosław Janaszek <jaroslaw.janaszek@gmail.com>
2019-06-25 18:44:23 +0200

    class - BEM

--- main.js
+++ main.js
@@ -4,3 +4,7 @@ const age = 27;
 console.log(name);
 console.log(age);
 console.log(`Nazywam się ${name} i mam ${age} lat`);
+
+const about = document.querySelector(`.about__paragraph--js`);
+console.log(about);
+about.innerHTML = `Nazywam się ${name} i mam ${age} lat`;
