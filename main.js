f8a90c61313b21dff0baefe8c448680d9f3a3c75
=Jarosław Janaszek <jaroslaw.janaszek@gmail.com>
2019-06-26 18:28:08 +0200

    navigation, table + normalize CSS

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 console.log('siema');
 const name = 'Maciek';
-const age = 27;
+const age = 17;
 console.log(name);
 console.log(age);
 console.log(`Nazywam się ${name} i mam ${age} lat`);
@@ -8,3 +8,40 @@ console.log(`Nazywam się ${name} i mam ${age} lat`);
 const about = document.querySelector(`.about__paragraph--js`);
 console.log(about);
 about.innerHTML = `Nazywam się ${name} i mam ${age} lat`;
+
+if ('javascript' != 'java') {
+  console.log(`to prawda!`)
+}
+if (age < 20) {
+  console.log('masz mniej niż 20 lat');
+} else if (age >= 20 && age <= 30) {
+  console.log('masz więcej niż 20 lat ale mniej niż 30');
+} else {
+  console.log('jesteś 31+');
+}
+
+switch (age) {
+  case 20:
+    console.log('masz 20 lat');
+    break;
+  case 30:
+    console.log('masz 30 lat');
+    break;
+  default:
+    console.log(`masz ${age} lat`);
+    break;
+}
+
+const amIOld = (age > 70) ? 'yes' : 'no';
+console.log(amIOld);
+
+function calculate(x) {
+  x = x + 3;
+  console.log(`trafycyjnie ${x}`);
+  return x*7;
+}
+
+console.log(calculate(2));
+
+const calculateFat = x => (x+3)*7;
+console.log(calculateFat(2));
